3ff6fcb0e19109ed73c7699670b590a4065957cd
Egert Gjyla <egertgjyla1995@gmail.com>
2018-03-07 16:20:27 +0100

    Added the List of songs

--- src/App.js
+++ src/App.js
@@ -2,17 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class List extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      songs: [
+        {name: 'The End', artist: 'The Doors'},
+        {name: 'National Anthem', artist: 'Radiohead'},
+        {name: 'Blowin\' in the wind', artist: 'Bob Dylan'},
+        {name: 'Time', artist: 'Pink Floyd'},
+        {name: 'Money', artist: 'Pink Floyd'},
+        {name: 'Into You', artist: 'Chat Faker'},
+        {name: 'You Don\'t Know Me', artist: 'Son Lux'}        
+      ]
+    }
+  }
+    render() {
+      return (
+        <div>
+          <ul>
+            {this.state.songs.map(song => 
+              <li>{song.name} by {song.artist}</li>
+            )}
+          </ul>
+        </div>
+      )
+    }
+  }
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React, Egert</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <List />
       </div>
     );
   }
